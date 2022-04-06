f36da328837f2546b6e8a3a1b2093cb8e7152c7e
dacortess <70292031+dacortess@users.noreply.github.com>
2022-04-06 13:37:13 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-palabras = ["<a ghhj href=\"refref\" ageadg> hola </a gjg jfdh>"]
-
-patron = re.compile("href=\".*?\"")
-for palabra in palabras:
-    m = patron.findall(palabra)[0][6:-1]
-    print(m)
